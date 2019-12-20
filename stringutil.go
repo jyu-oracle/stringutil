a99e5ca0ad8de2998e4c8adc55774aa8a80d78af
jason <jrubber@gmail.com>
2019-12-20 16:40:52 +0900

    updated

--- stringutil.go
+++ stringutil.go
@@ -18,6 +18,8 @@ func stripDoubleQuote(str string) string {
 	return str
 }
 
+// ParseKeyValuePairs returns key-value pairs directly parsed from the input string.
+// Expected input pattern : key  [=|:]  ( "value" | value )
 func ParseKeyValuePairs(line string, prefix string) map[string]string {
 	if prefix != "" {
 		idx := strings.Index(line, prefix)
@@ -32,14 +34,14 @@ func ParseKeyValuePairs(line string, prefix string) map[string]string {
 	retMap := make(map[string]string)
 
 	/*
-		key  [=|:]  ( "value" | value ) {,}
-		\s* ([^\s,]+) \s*  [=|:]  \s* ( ("[^"]+") | ([^\s,]+) )  [,]?
+		key  [=|:]  ( "value" | value )
+		([^\s]+) \s* [=|:] \s* ( ("[^"]+") | ([^\s,]+) )
 
 		\s             whitespace (== [\t\n\f\r ])
 		"[^"]+"        double quoted string
 		[^\s,]+        string not containing whitespace and ',' character
 	*/
-	rex := regexp.MustCompile(`\s*([^\s,]+)\s*[=|:]\s*(("[^"]+")|([^\s,]+))[,]?`)
+	rex := regexp.MustCompile(`([^\s]+)\s*[=|:]\s*(("[^"]+")|([^\s,]+))`)
 	data := rex.FindAllStringSubmatch(line, -1)
 
 	for _, v := range data {
@@ -53,6 +55,11 @@ func ParseKeyValuePairs(line string, prefix string) map[string]string {
 	return retMap
 }
 
+// ParseValues returns key-value pairs.
+// Keys are explicitly provided as fields param, and values are parsed from the input string.
+// If specify "_" string for fields, the default key value ("#" + index) will be used. Otherwise the specified field value will be used as key.
+// For the case of value which doesn't have matching key, the default key("#" + index) is used.
+// Expected input pattern : ( "value" | value )
 func ParseValues(line string, prefix string, fields []string) map[string]string {
 	if prefix != "" {
 		idx := strings.Index(line, prefix)
@@ -67,19 +74,19 @@ func ParseValues(line string, prefix string, fields []string) map[string]string 
 	retMap := make(map[string]string)
 
 	/*
-		( "value" | value ) {,}
-		\s* ( ("[^"]+") | ([^\s,]+) )  [,]?
+		( "value" | value )
+		( ("[^"]+") | ([^\s,]+) )
 
 		\s             whitespace (== [\t\n\f\r ])
 		"[^"]+"        double quoted string
 		[^\s,]+        string not containing whitespace and ',' character
 	*/
-	rex := regexp.MustCompile(`\s*(("[^"]+")|([^\s,]+))[,]?`)
+	rex := regexp.MustCompile(`(("[^"]+")|([^\s,]+))`)
 	data := rex.FindAllStringSubmatch(line, -1)
 
 	for i, v := range data {
 		var key string
-		if i < len(fields) {
+		if i < len(fields) && fields[i] != "_" {
 			key = fields[i]
 		} else {
 			key = fmt.Sprintf("#%d", i+1)
@@ -88,6 +95,58 @@ func ParseValues(line string, prefix string, fields []string) map[string]string 
 		retMap[key] = value
 		if debug {
 			fmt.Printf("[%v]->[%v]\n", key, value)
+		}
+	}
+	return retMap
+}
+
+// ParseData returns key-value pairs and values parsed from the input string.
+// fields param is used to supply additional information for keys. If specify "_" string for fields, the default key value (key value parsed from input or #idx) will be used. Otherwise the specified field value will override the default key value.
+// Expected input pattern : ( { key  [=|:] }  ( "value" | value ) )
+func ParseData(line string, prefix string, fields []string) map[string]string {
+	if prefix != "" {
+		idx := strings.Index(line, prefix)
+		if idx == -1 {
+			return nil
+		}
+		line = line[idx+len(prefix):]
+	}
+	if debug {
+		fmt.Println(line)
+	}
+	retMap := make(map[string]string)
+
+	rex1 := regexp.MustCompile(`([^\s]+)`)
+	rex2 := regexp.MustCompile(`([^\s]+)[=|:](("[^"]+")|([^\s,]+))`)
+	data1 := rex1.FindAllStringSubmatch(line, -1)
+
+	for i, v := range data1 {
+		var key string
+		value := stripDoubleQuote(v[1])
+		data2 := rex2.FindAllStringSubmatch(value, -1)
+		if len(data2) == 0 {
+			if i < len(fields) && fields[i] != "_" {
+				key = fields[i]
+			} else {
+				key = fmt.Sprintf("#%d", i+1)
+			}
+			retMap[key] = value
+			if debug {
+				fmt.Printf("[%v]->[%v]\n", key, value)
+			}
+		} else {
+			for _, vv := range data2 {
+				if i < len(fields) && fields[i] != "_" {
+					key = fields[i]
+				} else {
+					key = vv[1]
+				}
+				value = stripDoubleQuote(vv[2])
+				retMap[key] = value
+				if debug {
+					fmt.Printf("* [%v]->[%v]\n", key, value)
+				}
+			}
 		}
 	}
 	return retMap
---
+++ stringutil_test.go
@@ -0,0 +1,112 @@
+package stringutil
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestParseKeyValuePairs(t *testing.T) {
+	cases := []struct {
+		line   string
+		prefix string
+		m      map[string]string
+	}{
+		{"prefix key1=val1 key2=val2", "prefix", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix key1=val1, key2=val2", "prefix", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix key1=val1  , key2=val2  ,", "prefix", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix key1 = val1 key2 = val2", "prefix", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix key1 : val1 key2 : val2", "prefix", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix key1:val1 key2:val2", "prefix", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix key1:val1, key2:val2", "prefix", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix key1:val1    key2:val2   ", "prefix", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix key1:val1, key2:val2, key3:val3, key4:val4", "prefix", map[string]string{"key1": "val1", "key2": "val2", "key3": "val3", "key4": "val4"}},
+		{"key1=val1 key2=val2", "", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix key1=val1 key2=val2", "NOMATCH", nil},
+		{"prefixkey1=val1 key2=val2", "prefix", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"key0=val0 prefix key1=val1 key2=val2", "prefix", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix", "prefix", map[string]string{}},
+		{`prefix key1="val1, from here to there" key2=val2`, "prefix", map[string]string{"key1": "val1, from here to there", "key2": "val2"}},
+		{`prefix key1="val1, from here to there", key2=val2`, "prefix", map[string]string{"key1": "val1, from here to there", "key2": "val2"}},
+		{`prefix key1="val1, key2=val2`, "prefix", map[string]string{"key1": `"val1`, "key2": "val2"}},
+		{`waveform=0x101 (auto) mode=0x0 temp:4096 update region top=576, left=19, width=562, height=69 flags=0x0`, "", map[string]string{"waveform": "0x101", "mode": "0x0", "temp": "4096", "top": "576", "left": "19", "width": "562", "height": "69", "flags": "0x0"}},
+		{`update end marker=122, end time=1558318772259, time taken=299 ms`, "", map[string]string{"marker": "122", "time": "1558318772259", "taken": "299"}},
+		{`X11EM.onPointerEvent() RELEASED btn=1, (300,147) time=1558318772034`, "", map[string]string{"btn": "1", "time": "1558318772034"}},
+		{"prefix key1=val1 key2=val2 key1=val3", "prefix", map[string]string{"key1": "val3", "key2": "val2"}},
+	}
+
+	for _, c := range cases {
+		r := ParseKeyValuePairs(c.line, c.prefix)
+		if debug {
+			fmt.Println(r)
+		}
+		if !reflect.DeepEqual(c.m, r) {
+			t.Errorf("ParseKeyValuePairs(%v,%v)=%v, want %v", c.line, c.prefix, r, c.m)
+		}
+	}
+}
+
+func TestParseValues(t *testing.T) {
+	cases := []struct {
+		line   string
+		prefix string
+		fields string
+		m      map[string]string
+	}{
+		{"prefix val1 val2", "prefix", "key1 key2", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix val1, val2", "prefix", "key1 key2", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix val1   ,   val2", "prefix", "key1 key2", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix val1 val2 val3 val4", "prefix", "", map[string]string{"#1": "val1", "#2": "val2", "#3": "val3", "#4": "val4"}},
+		{"prefix val1 val2 val3 val4", "prefix", "key1 key2", map[string]string{"key1": "val1", "key2": "val2", "#3": "val3", "#4": "val4"}},
+		{"prefix val1 val2", "NOMATCH", "key1 key2", nil},
+		{"prefixval1 val2", "prefix", "key1 key2", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"uselessvalues prefix val1 val2", "prefix", "key1 key2", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix", "prefix", "", map[string]string{}},
+		{`prefix "val1, from here to there" val2`, "prefix", "key1 key2", map[string]string{"key1": "val1, from here to there", "key2": "val2"}},
+		{`prefix "val1, from here to there", val2`, "prefix", "key1 key2", map[string]string{"key1": "val1, from here to there", "key2": "val2"}},
+		{`prefix "val1 val2`, "prefix", "key1 key2", map[string]string{"key1": `"val1`, "key2": "val2"}},
+		{"prefix val0 val1 val2 val3", "prefix", "_ key1 key2 key3", map[string]string{"#1": "val0", "key1": "val1", "key2": "val2", "key3": "val3"}},
+	}
+
+	for _, c := range cases {
+		r := ParseValues(c.line, c.prefix, strings.Fields(c.fields))
+		if debug {
+			fmt.Println(r)
+		}
+		if !reflect.DeepEqual(c.m, r) {
+			t.Errorf("ParseValues(%v,%v,%v)=%v, want %v", c.line, c.prefix, c.fields, r, c.m)
+		}
+	}
+}
+
+func TestParseData(t *testing.T) {
+	cases := []struct {
+		line   string
+		prefix string
+		fields string
+		m      map[string]string
+	}{
+		{"prefix key1=val1 val2 key3=val3", "prefix", "_ key2 _", map[string]string{"key1": "val1", "key2": "val2", "key3": "val3"}},
+		{"prefix key1:val1 val2 key3:val3", "prefix", "_ key2 _", map[string]string{"key1": "val1", "key2": "val2", "key3": "val3"}},
+		{"prefix key1:val1 val2 key3:val3, key4:val4", "prefix", "_ key2 _", map[string]string{"key1": "val1", "key2": "val2", "key3": "val3", "key4": "val4"}},
+		{"prefix key1=val1 val2 key3=val3", "prefix", "MyKey1 key2 MyKey3", map[string]string{"MyKey1": "val1", "key2": "val2", "MyKey3": "val3"}},
+		{"prefix key1=val1 val2 key3=val3", "prefix", "_ _ _", map[string]string{"key1": "val1", "#2": "val2", "key3": "val3"}},
+		{"prefix key1=val1 val2 key3=val3", "prefix", "", map[string]string{"key1": "val1", "#2": "val2", "key3": "val3"}},
+		{`X11EM.onPointerEvent() RELEASED btn=1, (300,147) time=1558318772034`, "", "evt subtype _ pos _", map[string]string{"evt": "X11EM.onPointerEvent()", "subtype": "RELEASED", "btn": "1", "pos": "(300,147)", "time": "1558318772034"}},
+		{`perfScenario CVM received X11 ButtonRelease button=1 time=1560928559.821221`, "CVM received", "_ evt", map[string]string{"#1": "X11", "evt": "ButtonRelease", "button": "1", "time": "1560928559.821221"}},
+		{`prefix key1="val1", val2`, "prefix", "_ key2", map[string]string{"key1": "val1", "key2": "val2"}},
+		{`prefix key1=val1, "val2"`, "prefix", "_ key2", map[string]string{"key1": "val1", "key2": "val2"}},
+		{"prefix val1 val2", "NOMATCH", "key1 key2", nil},
+	}
+
+	for _, c := range cases {
+		r := ParseData(c.line, c.prefix, strings.Fields(c.fields))
+		if debug {
+			fmt.Println(r)
+		}
+		if !reflect.DeepEqual(c.m, r) {
+			t.Errorf("ParseData(%v, %v, %v)=%v, want %v", c.line, c.prefix, c.fields, r, c.m)
+		}
+	}
+}
